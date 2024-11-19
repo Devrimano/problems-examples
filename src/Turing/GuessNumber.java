359c890cd58ecf7800aee645e9468213ef89e259
Devrim-afk <149075796+Devrim-afk@users.noreply.github.com>
2024-11-19 15:02:58 +0400

    guess number game commit

---
+++ src/Turing/GuessNumber.java
@@ -0,0 +1,52 @@
+package Turing;
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class GuessNumber {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("please enter length of number");
+        int lengthOfNumber = sc.nextInt();
+        int attemptCounter = 0;
+        int low = (int) Math.pow(10, lengthOfNumber - 1);
+        int hi = low * 10;
+        int randomNumber = new Random().nextInt(low, hi);
+        System.out.println(randomNumber);
+        int[] userArr = new int[lengthOfNumber];
+        int[] randomNumberArr = new int[lengthOfNumber];
+        for (int i = randomNumberArr.length-1; i>=0; i--) {
+            randomNumberArr[i] = randomNumber%10;
+            randomNumber /=10;
+        }
+        while (true) {
+            attemptCounter++;
+            int yellowCounter = 0;
+            int greenCounter = 0;
+            for (int i = 0; i < userArr.length; i++) {
+                userArr[i] = sc.nextInt();
+            }
+
+            for (int i = 0; i < userArr.length; i++) {
+                if(userArr[i] == randomNumberArr[i]){
+                    greenCounter++;
+                }
+                else{
+                    for (int j = 0; j < randomNumberArr.length; j++) {
+                        if(userArr[i] ==randomNumberArr[j] && i !=j ){
+                            yellowCounter++;
+                        }
+                    }
+                }
+            }
+
+            if(greenCounter == randomNumberArr.length){
+                System.out.println("you win" + "," + "attempt: " + attemptCounter);
+                break;
+            }
+            else
+                System.out.println(greenCounter + "green" + ","+ yellowCounter+ "yellow");
+        }
+    }
+}
